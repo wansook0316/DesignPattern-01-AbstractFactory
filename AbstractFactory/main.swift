59e3c4d61adef05f24d5f19400746c99100380f1
wansook0316 <wansook0316@gmail.com>
2022-08-09 14:07:52 +0900

    Update: 외부에서 운영체제 값을 읽어와 component를 변경하는 main 함수 작성

--- AbstractFactory/main.swift
+++ AbstractFactory/main.swift
@@ -7,5 +7,30 @@
 
 import Foundation
 
-print("Hello, World!")
+internal func main(args: [String]) {
+    
+    // 특정 운영체제에 의존적인 코드가 없음
+    let factory = determineProductType(by: args.first ?? "")
+    
+    let button = factory.createButton(caption: "확인")
+    var checkBox = factory.createCheckBox(isChecked: true)
+    var textEdit = factory.createTextEdit(value: "디자인 패턴")
+    
+    button.render()
+    checkBox.render()
+    textEdit.render()
+    
+    button.clickEvent()
+    checkBox.updateIsChecked(to: false)
+    textEdit.updateValue(with: "오우예")
+}
 
+internal func determineProductType(by os: String) -> ComponentFactory {
+    if os == "windows" {
+        return WindowsFactory()
+    } else {
+        return LinuxFactory()
+    }
+}
+
+main(args: ["windows"])
