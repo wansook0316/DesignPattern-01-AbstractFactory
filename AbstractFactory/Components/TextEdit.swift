5e71c7d605c0937300b4d4be8b8bf893f086ff2e
wansook0316 <wansook0316@gmail.com>
2022-08-09 13:28:16 +0900

    Create: TextEdit 추가

---
+++ AbstractFactory/Components/TextEdit.swift
@@ -0,0 +1,27 @@
+//
+//  TextEdit.swift
+//  AbstractFactory
+//
+//  Created by Choiwansik on 2022/08/09.
+//
+
+import Foundation
+
+internal protocol TextEdit {
+    
+    var value: String { get set }
+    
+    func updateValue(with value: String)
+    func render()
+    
+}
+
+extension TextEdit {
+    
+    internal mutating func updateValue(with value: String) {
+        self.value = value
+        self.render()
+    }
+    
+}
+
