647dbf7f28213c49e1c8df925f71dda260d1d0cb
wansook <wansook0316@gmail.com>
2022-08-09 13:15:30 +0900

    Create: CheckBox interface 추가

---
+++ AbstractFactory/Components/CheckBox.swift
@@ -0,0 +1,27 @@
+//
+//  CheckBox.swift
+//  AbstractFactory
+//
+//  Created by Choiwansik on 2022/08/09.
+//
+
+import Foundation
+
+internal protocol CheckBox {
+
+    var isChecked: Bool { get set }
+
+    func updateIsChecked(to isChecked: Bool)
+    func render()
+
+}
+
+extension CheckBox {
+
+    // 환경에 따라 상관없이 해주어야 하는 동작을 정의
+    internal mutating func updateIsChecked(to isChecked: Bool) {
+        self.isChecked = isChecked
+        self.render()
+    }
+
+}
