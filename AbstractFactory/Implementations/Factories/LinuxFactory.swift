67d612cb310920fe05ed23bfbe5010e96e0de53e
wansook0316 <wansook0316@gmail.com>
2022-08-09 13:52:40 +0900

    Create: Window, Linux Factory 추가

---
+++ AbstractFactory/Implementations/Factories/LinuxFactory.swift
@@ -0,0 +1,8 @@
+//
+//  LinuxFactory.swift
+//  AbstractFactory
+//
+//  Created by Choiwansik on 2022/08/09.
+//
+
+import Foundation
---
+++ AbstractFactory/Implementations/Factories/WindowsFactory.swift
@@ -0,0 +1,8 @@
+//
+//  WindowsFactory.swift
+//  AbstractFactory
+//
+//  Created by Choiwansik on 2022/08/09.
+//
+
+import Foundation
